6f8b5299366015d9a7ae28d00f26342df31584db
Rongjian Zhang <pd4d10@gmail.com>
2019-08-20 22:22:50 +0800

    feat: add highlighter widget

--- flutter_highlight/lib/flutter_highlight.dart
+++ flutter_highlight/lib/flutter_highlight.dart
@@ -1,7 +1,43 @@
-library flutter_highlight;
+import 'package:flutter/widgets.dart';
+import 'package:highlight/highlight.dart';
+import 'package:highlight/languages/all.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Highlighter extends StatelessWidget {
+  static final _h = Highlight();
+
+  final String code;
+
+  Highlighter(this.code);
+
+  List<TextSpan> convert(List<Node> nodes) {
+    List<TextSpan> spans = [];
+
+    traverse(Node node) {
+      if (node.value != null) {
+        if (node.className != null) {
+          spans.add(TextSpan(
+              text: node.value,
+              style: TextStyle(
+                color: Color(0xff122345),
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.normal,
+              )));
+        } else {
+          spans.add(TextSpan(text: node.value));
+        }
+      } else if (node.children != null) {
+        node.children.forEach(traverse);
+      }
+    }
+
+    nodes.forEach(traverse);
+    return spans;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    all.forEach(_h.registerLanguage);
+    var nodes = _h.highlight(code).value;
+    return RichText(text: TextSpan(children: convert(nodes)));
+  }
 }
