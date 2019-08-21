6ae2549a7f1c929069a96208aed74b07d08c17cd
Rongjian Zhang <pd4d10@gmail.com>
2019-08-21 16:40:41 +0800

    fix: flutter highlight traverse

--- flutter_highlight/lib/flutter_highlight.dart
+++ flutter_highlight/lib/flutter_highlight.dart
@@ -6,38 +6,51 @@ class Highlighter extends StatelessWidget {
   static final _h = Highlight();
 
   final String code;
+  final String language;
+  final Map<String, TextStyle> style;
 
-  Highlighter(this.code);
+  Highlighter(this.code, {this.language, this.style = const {}});
 
   List<TextSpan> convert(List<Node> nodes) {
     List<TextSpan> spans = [];
+    var currentSpans = spans;
+    List<List<TextSpan>> stack = [];
 
-    traverse(Node node) {
+    _traverse(Node node) {
       if (node.value != null) {
-        if (node.className != null) {
-          spans.add(TextSpan(
-              text: node.value,
-              style: TextStyle(
-                color: Color(0xff122345),
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.normal,
-              )));
-        } else {
-          spans.add(TextSpan(text: node.value));
-        }
+        currentSpans.add(node.className == null
+            ? TextSpan(text: node.value)
+            : TextSpan(text: node.value, style: style[node.className]));
       } else if (node.children != null) {
-        node.children.forEach(traverse);
+        List<TextSpan> tmp = [];
+        currentSpans.add(TextSpan(children: tmp, style: style[node.className]));
+        stack.add(currentSpans);
+        currentSpans = tmp;
+
+        node.children.forEach((n) {
+          _traverse(n);
+          if (n == node.children.last) {
+            currentSpans = stack.isEmpty ? spans : stack.removeLast();
+          }
+        });
       }
     }
 
-    nodes.forEach(traverse);
+    for (var node in nodes) {
+      _traverse(node);
+    }
+
     return spans;
   }
 
   @override
   Widget build(BuildContext context) {
-    all.forEach(_h.registerLanguage);
-    var nodes = _h.highlight(code).value;
-    return RichText(text: TextSpan(children: convert(nodes)));
+    all.forEach(_h.registerLanguage); // FIXME:
+
+    var nodes = _h.highlight(code, language: language).value;
+    return RichText(
+        text: TextSpan(
+            children: convert(nodes),
+            style: TextStyle(color: Color(0xff000000), fontFamily: 'Menlo')));
   }
 }
