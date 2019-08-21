eb03b4809c618ad9876fbb099c6cb65b0df1727c
Rongjian Zhang <pd4d10@gmail.com>
2019-08-21 14:47:44 +0800

    build: add background color, bug fixes

--- tools/style.js
+++ tools/style.js
@@ -9,6 +9,50 @@ const postcss = require("postcss");
 const rootDir = path.resolve(__dirname, "node_modules/highlight.js/styles");
 const destDir = path.resolve(__dirname, "../flutter_highlight/lib/styles");
 
+/**
+ * white, #fff, #ffffff, rgba(0,0,0,0) -> Flutter color
+ *
+ * @param {string} color
+ */
+const covertColor = color => {
+  if (color === "inherit") {
+    // FIXME:
+    return;
+  }
+
+  if (color.includes("url(")) {
+    // FIXME:
+    return;
+  }
+
+  if (color.startsWith("rgba(")) {
+    return `Color.fromRGBO${color.slice(4)}`;
+  } else {
+    let rgb = "";
+
+    if (color === "white") {
+      rgb = "ffffff";
+    } else if (color === "black") {
+      rgb = "000000";
+    } else if (color === "navy") {
+      rgb = "000080";
+    } else if (color === "gold") {
+      rgb = "ffd700";
+    } else if (color.startsWith("#")) {
+      rgb = color.slice(1);
+      if (rgb.length === 3) {
+        rgb = rgb
+          .split("")
+          .map(x => x + x)
+          .join("");
+      }
+      return `Color(0xff${rgb})`;
+    } else {
+      console.log(`color ignored: ${color}`);
+    }
+  }
+};
+
 // ["github.css"]
 fs.readdirSync(rootDir).forEach(file => {
   if (path.extname(file) != ".css") return;
@@ -19,48 +63,55 @@ fs.readdirSync(rootDir).forEach(file => {
   let code = "import 'package:flutter/painting.dart'; var style = {";
 
   ast.walkRules((rule, index) => {
-    // const keyword = rule.selector.slice(6); // remove .hljs-
-    // console.log(keyword);
-    // const selectors = rule.selector.split(".");
-    // console.log(rule.selectors);
-    // console.log(rule.nodes[0].type === "decl");
+    // FIXME: a11y media query
+    if (rule.parent.type === "atrule" && rule.parent.name === "media") {
+      return;
+    }
 
     rule.selectors.forEach(selector => {
-      selector = selector.trim().replace(".hljs-", "");
-      code += `'${selector}': TextStyle(`;
-
-      const item = rule.nodes[0];
-
-      if (item.type === "decl") {
-        switch (item.prop) {
-          case "color":
-            let rgb = "";
-            if (item.value.startsWith("#")) {
-              rgb = item.value.slice(1);
-              if (rgb.length === 3) {
-                rgb = rgb
-                  .split("")
-                  .map(x => x + x)
-                  .join("");
-              }
-            } else {
-              console.log(item.value);
-            }
-            code += `color: Color(0xff${rgb}),`;
-            break;
-          case "font-style":
-            assert(item.value === "italic");
-            code += `fontStyle: FontStyle.italic,`;
-            break;
-          case "font-weight":
-            code += `fontWeight: FontWeight.${item.value},`;
-            break;
-        }
-      } else {
-        throw new Error();
+      if (/\s+/.test(selector)) {
+        // FIXME: nested selector
+        selector = selector.split(/\s+/).slice(-1)[0];
       }
+      selector = selector.replace(".hljs-", "").replace(".hljs", "container");
 
-      code += "),";
+      const styles = [];
+      rule.nodes.forEach(item => {
+        if (item.type === "decl") {
+          switch (item.prop) {
+            case "color": {
+              const flutterColor = covertColor(item.value);
+              if (flutterColor) {
+                styles.push(`color: ${flutterColor}`);
+              }
+              break;
+            }
+            case "background":
+            case "background-color": {
+              const flutterColor = covertColor(item.value);
+              if (flutterColor) {
+                styles.push(`backgroundColor: ${flutterColor}`);
+              }
+              break;
+            }
+            case "font-style":
+              styles.push(`fontStyle: FontStyle.${item.value}`);
+              break;
+            case "font-weight":
+              if (item.value === "bolder") {
+                item.value = "bold"; // FIXME:
+              }
+              styles.push(`fontWeight: FontWeight.${item.value}`);
+              break;
+          }
+        } else {
+          console.log(`rule ignored: ${item.type}`);
+        }
+      });
+
+      if (styles.length) {
+        code += `'${selector}': TextStyle(${styles.join(",")}),`;
+      }
     });
   });
 
