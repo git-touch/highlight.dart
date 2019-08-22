103ca488f85a88fd20b38911855f742f8de5b0ab
Rongjian Zhang <pd4d10@gmail.com>
2019-08-22 21:01:36 +0800

    build: add test files copy script

--- tools/copy.js
+++ tools/copy.js
@@ -1,6 +1,26 @@
 const path = require("path");
+const { execSync } = require("child_process");
 const map = require("map-stream");
 const vfs = require("vinyl-fs");
+
+// Copy highlight test files
+execSync("rm -rf /tmp/highlight.js");
+execSync(
+  "git clone --branch 9.15.10 --depth 1 https://github.com/highlightjs/highlight.js.git /tmp/highlight.js"
+);
+
+vfs
+  .src("/tmp/highlight.js/test/{detect,markup}/**/*")
+  .pipe(
+    map((file, cb) => {
+      if (file.path.endsWith(".js")) {
+        cb(null);
+      } else {
+        cb(null, file);
+      }
+    })
+  )
+  .pipe(vfs.dest(path.resolve(__dirname, "../highlight/test")));
 
 /**
  * Copy flutter_highlight files to gallery
