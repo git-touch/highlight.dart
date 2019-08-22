2ea5cbf8b0b137e8aa46d79b8cb14de2dbe75a6d
Rongjian Zhang <pd4d10@gmail.com>
2019-08-22 21:34:37 +0800

    build: add code example script

--- tools/copy.js
+++ tools/copy.js
@@ -1,14 +1,15 @@
+const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 const map = require("map-stream");
 const vfs = require("vinyl-fs");
 
-// Copy highlight test files
 execSync("rm -rf /tmp/highlight.js");
 execSync(
   "git clone --branch 9.15.10 --depth 1 https://github.com/highlightjs/highlight.js.git /tmp/highlight.js"
 );
 
+// Copy highlight test files
 vfs
   .src("/tmp/highlight.js/test/{detect,markup}/**/*")
   .pipe(
@@ -21,6 +22,34 @@ vfs
     })
   )
   .pipe(vfs.dest(path.resolve(__dirname, "../highlight/test")));
+
+// Generate code example dart files
+let code = "var exampleMap = {";
+// ["dart"]
+fs.readdirSync(path.resolve(__dirname, "../highlight/test/detect")).forEach(
+  langName => {
+    const content = fs
+      .readFileSync(
+        path.resolve(
+          __dirname,
+          "../highlight/test/detect",
+          langName,
+          "default.txt"
+        ),
+        "utf8"
+      )
+      .replace(/\\/g, "\\\\")
+      .replace(/'/g, "\\'")
+      .replace(/\$/g, "\\$")
+      .replace(/\n/g, "\\n");
+    code += `'${langName}':'${content}',`;
+  }
+);
+code += "};";
+fs.writeFileSync(
+  path.resolve(__dirname, "../flutter_highlight_gallery/lib/example.dart"),
+  code
+);
 
 /**
  * Copy flutter_highlight files to gallery
