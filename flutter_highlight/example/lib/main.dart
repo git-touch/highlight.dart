b55f5d661ca8a696d54d77771dbca389cdd9f0ba
Rongjian Zhang <pd4d10@gmail.com>
2019-11-13 12:51:25 +0800

    chore: commit generated ios files

--- flutter_highlight/example/lib/main.dart
+++ flutter_highlight/example/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
-      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
   }
