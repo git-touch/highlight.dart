a3a3360b69b3630eaae83ff106f6ca4135ba784c
kvii <56432636+kvii@users.noreply.github.com>
2021-11-23 15:14:01 +0800

    add keyword 'required' in dart

--- highlight/lib/languages/dart.dart
+++ highlight/lib/languages/dart.dart
@@ -39,7 +39,7 @@ final dart = Mode(refs: {
   ]),
 }, keywords: {
   "keyword":
-      "abstract as assert async await break case catch class const continue covariant default deferred do dynamic else enum export extends extension external factory false final finally for Function get hide if implements import in inferface is library mixin new null on operator part rethrow return set show static super switch sync this throw true try typedef var void while with yield",
+      "abstract as assert async await break case catch class const continue covariant default deferred do dynamic else enum export extends extension external factory false final finally for Function get hide if implements import in inferface is library mixin new null on operator part required rethrow return set show static super switch sync this throw true try typedef var void while with yield",
   "built_in":
       "Comparable DateTime Duration Function Iterable Iterator List Map Match Null Object Pattern RegExp Set Stopwatch String StringBuffer StringSink Symbol Type Uri bool double dynamic int num print Element ElementList document querySelector querySelectorAll window"
 }, contains: [
